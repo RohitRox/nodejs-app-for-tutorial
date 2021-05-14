1473fbf46e8c547d00eb1edc2bcc8874cdd4190f
rojanjoshi <rozanjoshi@gmail.com>
2021-05-14 22:39:14 +0545

    Add /status route

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get("/", function(req, res) {
   res.send('Hello from Express!')
 })
 
+app.get("/status", function(req, res) {
+  res.send('Status Ok!')
+})
+
 app.listen(PORT, function() {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
