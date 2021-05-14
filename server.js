23b7cc3e67f94cb7955f3f9615937d8a66fa6c62
Rohit Joshi <rohit.joshi@cloudfactory.com>
2021-05-14 18:37:44 +0100

    Simple full stack app with layouts

--- server.js
+++ server.js
@@ -1,15 +1,46 @@
 const express = require("express")
+const expressLayouts = require('express-ejs-layouts')
 
 const app = express()
+app.use(express.static('public'))
+app.use(expressLayouts)
+app.set('view engine', 'ejs');
 
 const PORT = 4000
 
-app.get("/", function(req, res) {
-  res.send('Hello from Express!')
+app.get("/status", function(req, res) {
+  res.json({
+    status: "OK"
+  })
 })
 
-app.get("/status", function(req, res) {
-  res.send('Status Ok!')
+
+const products = [
+  {
+    id: 1,
+    name: "Kitchen Towel",
+    price: 112.5
+  },
+  {
+    id: 2,
+    name: "Paper Towel",
+    price: 60
+  },
+  {
+    id: 3,
+    name: "Spoon Set",
+    price: 320.5
+  }
+]
+
+app.get("/products", function(req, res) {
+  res.render('products', { products: products });
+})
+
+app.get("/products/:id", function(req, res) {
+  const productId = parseInt(req.params.id)
+
+  res.render('product', { product: products[productId-1] });
 })
 
 app.listen(PORT, function() {
